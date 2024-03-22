94578ff372b4823d2fedcdb3051f15e86034c281
perazabg <96875802+perazabg@users.noreply.github.com>
2024-03-22 02:19:09 -0700

    comments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+/*
+  each item in the todo list should be an object with the following properties:
+    - boolean isCompleted
+    - title
+    - description
+    - priority (e.g. high, medium, low)
+    - date created
+    - date completed
+    - date due
+    - project folder containing more todo items
+    - edit todo items
+*/
